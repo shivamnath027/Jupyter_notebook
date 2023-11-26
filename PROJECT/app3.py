2594ad54d0c46942911242e44d1f74dc18ece60a
Shivam Nath <shivamnath027@gmail.com>
2023-11-26 20:50:55 +0530

    Project

--- PROJECT/app3.py
+++ PROJECT/app3.py
@@ -8,7 +8,7 @@ import numpy as np
 # knn_gest = pickle.load(pickle_in_gestation)
 
 # Load the heart disease model
-pickle_in_heart = open("diabetes.pkl", "rb")
+pickle_in_heart = open("Diabetes.pkl", "rb")
 rf_diab = pickle.load(pickle_in_heart)
 
 # Set page configurations
@@ -33,7 +33,7 @@ if selected == "Gestation Diabetes Prediction":
     st.header("Gestational Diabetes Prediction")
 
 
-def predict_note_authentication(age, bmi, fpg, ffpg, sbp,dbp, smoking, drinking, alt):
+def predict_note_authentication(age, bmi, fpg, ffpg, sbp,dbp,family_history, smoking, drinking, alt):
     
     """Let's predict the disease 
     This is using docstrings for specifications.
@@ -63,7 +63,7 @@ def predict_note_authentication(age, bmi, fpg, ffpg, sbp,dbp, smoking, drinking,
         in: query
         type: number
         required: true
-      - name:dbp
+      - name:family_history
         in: query
         type: number
         required: true
@@ -98,11 +98,6 @@ def predict_note_authentication(age, bmi, fpg, ffpg, sbp,dbp, smoking, drinking,
       smoking = int(smoking)
       drinking = int(drinking)
       alt = float(alt)
-      Dia_BP = float(Dia_BP)
-      OGTT = int(OGTT)
-      Hemoglobin = float(Hemoglobin)
-      Sedentary_Lifestyle = int(Sedentary_Lifestyle)
-      Prediabetes = int(Prediabetes)
 
 
       # Use numpy array for prediction
@@ -130,18 +125,18 @@ col1, col2, col3 = st.columns(3)
 # col1
 with col1:
     age = st.number_input("age", min_value=0, max_value=100, placeholder="Enter age")
-    bmi = st.number_input("bmi", min_value=0, max_value=10, key="bmi", placeholder="Type Here")
+    bmi = st.number_input("bmi", min_value=0, max_value=30, key="bmi", placeholder="Type Here")
     fpg = st.number_input("fpg", min_value=0, max_value=100, key="fpg", placeholder="Type Here")
-    ffpg = st.number_input("ffpg", min_value=10.0, max_value=40.0, key="ffpg", placeholder="Type Here")
-    sbp = st.number_input("sbp", min_value=20, max_value=100, key="sbp", placeholder="Type Here")
+    ffpg = st.number_input("ffpg", min_value=0.0, max_value=40.0, key="ffpg", placeholder="Type Here")
+    sbp = st.number_input("sbp", min_value=20, max_value=150, key="sbp", placeholder="Type Here")
 
 # col2
 with col2:    
-    dbp = st.number_input("dbp", min_value=0, max_value=1, key="dbp", placeholder="Type Here")
+    dbp = st.number_input("dbp", min_value=1, max_value=100, key="dbp", placeholder="Type Here")
     family_history = st.number_input("family_history", min_value=0, max_value=10, key="family_history", placeholder="Type Here")
-    smoking = st.number_input("smoking", min_value=0, max_value=1, key="smoking", placeholder="Type Here")
-    drinking = st.number_input("drinking", min_value=0, max_value=1, key="drinking", placeholder="Type Here")
-    alt = st.number_input("alt", min_value=80, max_value=180, key="alt", placeholder="Type Here")
+    smoking = st.number_input("smoking", min_value=0, max_value=4, key="smoking", placeholder="Type Here")
+    drinking = st.number_input("drinking", min_value=0, max_value=4, key="drinking", placeholder="Type Here")
+    alt = st.number_input("alt", min_value=1, max_value=180, key="alt", placeholder="Type Here")
 
 
 
